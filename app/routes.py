e73647aadc1da43a0c1542a25f0c27457323c2e5
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-08-04 21:03:09 -0600

    Add templates for home page and navigation
    The base template includes the basic structure of an HTML page and the
    navigation links.
    
    The index template inherits from the base template, adds some
    information about the user and shows all the current posts.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,28 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
-def index():
-    return "Hello, World!"
+def index() -> str:
+    """Render the home page for Microblog."""
+    # Mock objects to model some functionality of the site.
+    user = {"username": "Spaghetti"}
+    posts = [
+        {
+            "author": {"username": "John"},
+            "body": "Beautiful day in Santiago!",
+        },
+        {
+            "author": {"username": "Jane"},
+            "body": "The Superman movie was so cool!",
+        },
+    ]
+
+    return render_template(
+        "index.html",
+        title="Home",
+        user=user,
+        posts=posts,
+    )
