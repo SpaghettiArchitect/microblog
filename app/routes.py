beec0f992f95efd3a143efa3b12dd0cda5ede15e
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-08-05 22:29:23 -0600

    Add login form and template page
    The new form is defined in the forms file, and it inherits from
    FlaskForm in the flask_wtf package. This external packege makes it easy
    to create new forms by only definig the type of input that the form will
    handle.
    
    A new login view function has been added to the routes file. This view
    shows a blank login form when the user first requests this page,
    validates the data submitted and redirects to the index page if
    everything is correct.
    
    The login template renders a simple form based on the class variables
    defined in LoginForm. If the form has any invalid fields when submitted,
    it shows all errors.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import Response, flash, redirect, render_template, url_for
 
 from app import app
+from app.forms import LoginForm
 
 
 @app.route("/")
@@ -26,3 +27,17 @@ def index() -> str:
         user=user,
         posts=posts,
     )
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login() -> Response | str:
+    """Render and validate the data on the login form."""
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash(
+            f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}"
+        )
+        return redirect(url_for("index"))
+
+    return render_template("login.html", title="Sign In", form=form)
