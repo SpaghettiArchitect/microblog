3f5447aa35ea125444451d19a7e959b71b162b56
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-08-07 23:32:28 -0600

    Add shell context to Flask
    For ease of use, some SQLAlchemy entities and database models are being
    imported into the shell context of Flask. This will import all modules,
    packages or classes needed to create and run queries in the shell.

--- microblog.py
+++ microblog.py
@@ -1,1 +1,13 @@
-from app import app  # noqa: F401
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+
+from app import app, db
+from app.models import Post, User
+
+
+@app.shell_context_processor
+def make_shell_context() -> dict:
+    """Returns symbols that can be used in the shell context when running
+    'flask shell'.
+    """
+    return {"sa": sa, "so": so, "db": db, "User": User, "Post": Post}
