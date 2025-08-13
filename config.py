cf5bbd01305a76b7dc3bf30d642459aaa4e04080
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-08-13 00:07:02 -0600

    Add error logging to emails
    The application is now able to send emails with logging information
    of any errors that occur. This emails are send to all administrators
    registered on the ADMIN environment variable.

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()  # Take all environment variables.
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -8,3 +12,9 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    ADMINS = [admin.strip() for admin in os.environ.get("ADMINS").split("\n")]
