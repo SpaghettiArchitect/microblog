e8f6a2c420d3dc11e16690b3cb19d3b7aae10633
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-08-13 00:36:42 -0600

    Add logging INFO to log file
    The application is now able to log all relevant information in the INFO
    category of Flask to the microblog.log file, in the logs directory. This
    data will be useful when debugging any kind of error.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler, SMTPHandler
+from pathlib import Path
 
 from flask import Flask
 from flask_login import LoginManager
@@ -15,24 +16,45 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"
 
-if not app.debug and app.config["MAIL_SERVER"]:
-    auth = None
-    if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
-        auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+if not app.debug:
+    if app.config["MAIL_SERVER"]:
+        auth = None
+        if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+            auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
 
-    secure = None
-    if app.config["MAIL_USE_TLS"]:
-        secure = ()
+        secure = None
+        if app.config["MAIL_USE_TLS"]:
+            secure = ()
 
-    mail_handler = SMTPHandler(
-        mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
-        fromaddr=f"no-reply@{app.config['MAIL_SERVER']}",
-        toaddrs=app.config["ADMINS"],
-        subject="Microblog Failure",
-        credentials=auth,
-        secure=secure,
+        mail_handler = SMTPHandler(
+            mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+            fromaddr=f"no-reply@{app.config['MAIL_SERVER']}",
+            toaddrs=app.config["ADMINS"],
+            subject="Microblog Failure",
+            credentials=auth,
+            secure=secure,
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    if not Path("logs").exists():
+        Path("logs").mkdir()
+
+    file_handler = RotatingFileHandler(
+        "logs/microblog.log",
+        maxBytes=10240,
+        backupCount=10,
     )
-    mail_handler.setLevel(logging.ERROR)
-    app.logger.addHandler(mail_handler)
+    file_handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+        )
+    )
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info("Microblog startup")
+
 
 from app import errors, models, routes  # noqa: E402, F401
