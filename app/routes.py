71b0c6abf84f4a4631301044eb84a72070f30945
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-08-20 20:31:10 -0600

    Add pagination links to index and explore pages
    Using the built-in pagination feature of Flask, this commit adds
    pagination links to the index and explore pages of the application. Now,
    users can navigate through multiple pages of posts more easily.

--- app/routes.py
+++ app/routes.py
@@ -42,12 +42,16 @@ def index() -> str:
         per_page=app.config["POSTS_PER_PAGE"],
         error_out=False,
     )
+    next_url = url_for("index", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for("index", page=posts.prev_num) if posts.has_prev else None
 
     return render_template(
         "index.html",
         title="Home Page",
         form=form,
         posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
     )
 
 
@@ -205,7 +209,18 @@ def explore() -> str:
     page = request.args.get("page", 1, type=int)
     query = sa.select(Post).order_by(Post.timestamp.desc())
     posts = db.paginate(
-        query, page=page, per_page=app.config["POSTS_PER_PAGE"], error_out=False
+        query,
+        page=page,
+        per_page=app.config["POSTS_PER_PAGE"],
+        error_out=False,
     )
+    next_url = url_for("explore", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for("explore", page=posts.prev_num) if posts.has_prev else None
 
-    return render_template("index.html", title="Explore", posts=posts.items)
+    return render_template(
+        "index.html",
+        title="Explore",
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
