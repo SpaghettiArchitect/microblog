2d596010976b555fd96cbcae30ee886c48618ee4
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-08-22 01:15:56 -0600

    Add asynchronous email sending functionality
    The `send_async_email` function has been added to allow sending emails
    without blocking the main thread. This way, the application can continue
    to handle requests while the email is being sent in the background.

--- app/email.py
+++ app/email.py
@@ -1,8 +1,16 @@
-from flask import render_template
+from threading import Thread
+
+from flask import Flask, render_template
 from flask_mail import Message
 
 from app import app, mail
 from app.models import User
+
+
+def send_async_email(app: Flask, msg: Message) -> None:
+    """Send an email asynchronously to avoid blocking the main thread."""
+    with app.app_context():
+        mail.send(msg)
 
 
 def send_email(
@@ -11,16 +19,15 @@ def send_email(
     recipients: list[str],
     text_body: str,
     html_body: str,
-):
-    """Send an email to a user or group of users with both plain text and
-    HTML content."""
+) -> None:
+    """Send an async email with the given subject, sender, recipients, and body."""
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
 
 
-def send_password_reset_email(user: User):
+def send_password_reset_email(user: User) -> None:
     """Send a password reset email to the user with a token."""
     token = user.get_reset_password_token()
     send_email(
